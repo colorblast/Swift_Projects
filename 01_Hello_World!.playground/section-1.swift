e773c98f985f216d7c3570c2889e9bd086020e44
thestuysu <thestuysu@gmail.com>
2015-03-25 21:16:08 -0400

    hello world

---
+++ 01_Hello_World!.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+println("Hello World!")
+var helloWorld = [1:"Starting Swift", 2:"Learning Swift", 3:"Making something with Swift"]
+let total = helloWorld.count
+
