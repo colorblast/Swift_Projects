3c5c9d875f147c4f8c2c8726ff030d438b2543c5
Jonathan Wong <18wongj@gmail.com>
2015-03-27 16:52:30 -0400

    Update section-1.swift

--- FizzBuzz Challenge.playground/section-1.swift
+++ FizzBuzz Challenge.playground/section-1.swift
@@ -1,19 +1,19 @@
 //Copyright Jonathan Wong 2015. All rights reserved.
 // Playground - noun: a place where people can play
 
-import UIKit
+Import UIKit
 
 //FizzBuzz is a children's game where a number divisible by 3 results in Fizz, divisible by 5 is Buzz, and divisible by both is FizzBuzz.
 
 var rand = 1...50
 
     for number in rand {
-            if number % 3 == 0 {
-                println("Fizz")
+            if number % 3 == 0 && number % 5 == 0 {
+                println("\(number). Wow. A FizzBuzz.")
             } else if number % 5 == 0 {
                 println("Buzz")
-            } else if number % 3 == 0 && number % 5 == 0 {
-                println("\(number). Wow. A FizzBuzz.")
+            } else if number % 3 == 0 {
+                println("Fizz")
             } else {
                 println("Nada. Lo siento. I'm sorry. \(number) is not divisible by 3 or 5.")
             }
