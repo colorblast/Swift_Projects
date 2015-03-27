0a0ac256563f56112f1eceb3f5b15b29d59a1266
thestuysu <thestuysu@gmail.com>
2015-03-27 16:42:26 -0400

    tuples

---
+++ 08_Decomp_Tuples.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func searchNames (#name: String) -> (found: Bool, description: String) {
+    let names =
+    ["Mark", "May", "Gary", "Allen", "Joy", "Dave"]
+    
+    var found = (false, "\(name) is not a name in our database.")
+    
+    for n in names {
+        if n == name {
+            found = (true, "\(name) is a name in our database.")
+        }
+    }
+    println("The name was not found. Value returned is \(found). Testing by Jonathan Wong.")
+    return found
+}
+
+let (found, description) = searchNames(name: "Luke")
+
+let result = searchNames(name: "Allen")
+result.found
+result.description
