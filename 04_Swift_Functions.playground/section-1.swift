1a7127605bf0d7af5cbee0bba2998c955d17e0ce
thestuysu <thestuysu@gmail.com>
2015-03-27 15:15:15 -0400

    functions

---
+++ 04_Swift_Functions.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+func calcArea() {
+    let h = 6
+    let w = 10
+    let area = h * w
+    println("The area of the space is \(area)")
+}
+
+calcArea()
