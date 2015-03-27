3bd599fb7f5e718020742e4ed02b6c0c849543a9
thestuysu <thestuysu@gmail.com>
2015-03-27 16:21:26 -0400

    edits

--- 07_Tuples.playground/section-1.swift
+++ 07_Tuples.playground/section-1.swift
@@ -4,7 +4,7 @@ import UIKit
 
 var str = "Hello, playground"
 
-func searchNames (#name: String) -> Bool, String {
+func searchNames (#name: String) -> (Bool, String) {
     let names =
     ["Mark", "May", "Gary", "Allen", "Joy", "Dave"]
     
@@ -12,7 +12,7 @@ func searchNames (#name: String) -> Bool, String {
     
     for n in names {
         if n == name {
-            found = true
+            found = (true, "\(name) is a name in our database.")
         }
     }
     println("The name was not found. Value returned is \(found). Testing by Jonathan Wong.")
