1dbeb770555f856833b6d87e349324e824f47190
Jonathan Wong <18wongj@gmail.com>
2015-04-02 13:57:46 -0400

    hello world

--- 01_Hello_World!.playground/section-1.swift
+++ 01_Hello_World!.playground/section-1.swift
@@ -2,9 +2,8 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
 println("Hello World!")
 var helloWorld = [1:"Starting Swift", 2:"Learning Swift", 3:"Making something with Swift"]
 let total = helloWorld.count
+println("\(total) items are for your todo list. These are \(helloWorld[1]),\(helloWorld[2]), and \(helloWorld[3]).")
 
